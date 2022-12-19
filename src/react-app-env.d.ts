8b9234d9f31317fd36865fda281736b2340bd680
himyjan <51815522+himyjan@users.noreply.github.com>
2022-12-19 10:06:09 +0800

    update package version

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare module '*.svg?inline' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
